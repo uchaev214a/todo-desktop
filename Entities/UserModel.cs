73725a55c43d0c13911b6963461dfa90f6c2c0e6
uchaev214a <dmitry.uchaev@mail.ru>
2025-02-20 13:04:52 +0300

    lab 3 saves

---
+++ Entities/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Pass { get; set; }
+        public string Name { get; set; }
+    }
+}
--- todo/Reg.xaml.cs
+++ todo/Reg.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using todo.Repository;
 
 namespace todo
 {
@@ -22,6 +23,33 @@ namespace todo
         public Reg()
         {
             InitializeComponent();
+        }
+
+        private void regbut2_Click(object sender, RoutedEventArgs e)
+        { 
+            var name = name_textbox.Text;
+            var email = email_textbox.Text;
+            var pass1 = pass1_textbox.Text;
+            var pass2 = pass2_textbox.Text;
+
+            try
+            {
+                UserRepository.GetInstance().Register(new Entities.UserModel() { Email = email, Name = name, Pass = pass1 }, pass2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            new MainEmpty().Show();
+            Close();
+        }
+
+        private void backbut1_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            Close();
         }
     }
 }
